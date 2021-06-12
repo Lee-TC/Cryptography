f92e130b73581f6f967f1eed1681dfa98883ac4d
Lee-TC <lithium.sea@gmail.com>
2021-06-12 22:15:47 +0800

    初始化

---
+++ ECC.py
@@ -0,0 +1,40 @@
+from Crypto.Util.number import inverse
+
+a = 497
+b = 1768
+p = 9739
+O = (0, 0)
+
+
+def point_addition(p_1, p_2):
+    if p_1 == O:
+        return p_2
+    elif p - 2 == O:
+        return p_1
+    else:
+        x_1, y_1 = p_1
+        x_2, y_2 = p_2
+        if (x_1, y_1) == (x_2, -y_2):
+            return O
+
+        if p_1 == p_2:
+            m = ((3 * x_1 * x_1 + a) * inverse(2 * y_1, p)) % p
+        else:
+            m = ((y_2 - y_1) * inverse(x_2 - x_1, p)) % p
+
+        x_3 = (m * m - x_2 - x_1) % p
+        y_3 = (m * (x_1 - x_3) - y_1) % p
+
+    return x_3, y_3
+
+
+def scalar_multiplication(P, n):
+    Q = P
+    R = O
+    while n > 0:
+        if n % 2 == 1:
+            R = point_addition(R, Q)
+        Q = point_addition(Q, Q)
+        n //= 2
+    return R
+
